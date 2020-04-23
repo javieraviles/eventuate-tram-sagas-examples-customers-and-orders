d2a436bcd11f76fcf9a8315407fe80722401cebb
Javier Aviles <avileslopez.javier@gmail.com>
2020-04-23 22:15:35 +0200

    Integrating product into order saga backend flow

--- order-backend/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/orders/common/OrderDetails.java
+++ order-backend/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/orders/common/OrderDetails.java
@@ -1,31 +1,43 @@
 package io.eventuate.examples.tram.sagas.ordersandcustomers.orders.common;
 
-import io.eventuate.examples.tram.sagas.ordersandcustomers.commondomain.Money;
-
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
+
+import io.eventuate.examples.tram.sagas.ordersandcustomers.commondomain.Money;
 
 @Embeddable
 public class OrderDetails {
 
-  private Long customerId;
+	private Long customerId;
+	private Long productId;
+	private int orderUnits;
 
-  @Embedded
-  private Money orderTotal;
+	@Embedded
+	private Money orderTotal;
 
-  public OrderDetails() {
-  }
+	public OrderDetails() {
+	}
 
-  public OrderDetails(Long customerId, Money orderTotal) {
-    this.customerId = customerId;
-    this.orderTotal = orderTotal;
-  }
+	public OrderDetails(Long customerId, Long productId, int orderUnits, Money orderTotal) {
+		this.customerId = customerId;
+		this.orderTotal = orderTotal;
+		this.productId = productId;
+		this.orderUnits = orderUnits;
+	}
 
-  public Long getCustomerId() {
-    return customerId;
-  }
+	public Long getCustomerId() {
+		return customerId;
+	}
 
-  public Money getOrderTotal() {
-    return orderTotal;
-  }
+	public Long getProductId() {
+		return productId;
+	}
+
+	public int getOrderUnits() {
+		return orderUnits;
+	}
+
+	public Money getOrderTotal() {
+		return orderTotal;
+	}
 }
--- order-backend/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/orders/sagas/createorder/CreateOrderSaga.java
+++ order-backend/src/main/java/io/eventuate/examples/tram/sagas/ordersandcustomers/orders/sagas/createorder/CreateOrderSaga.java
@@ -1,5 +1,7 @@
 package io.eventuate.examples.tram.sagas.ordersandcustomers.orders.sagas.createorder;
 
+import static io.eventuate.tram.commands.consumer.CommandWithDestinationBuilder.send;
+
 import io.eventuate.examples.tram.sagas.ordersandcustomers.commondomain.Money;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.customers.api.commands.ReserveCreditCommand;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.customers.api.replies.CustomerCreditLimitExceeded;
@@ -7,69 +9,82 @@ import io.eventuate.examples.tram.sagas.ordersandcustomers.customers.api.replies
 import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.domain.Order;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.domain.OrderRepository;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.orders.domain.RejectionReason;
+import io.eventuate.examples.tram.sagas.ordersandcustomers.products.api.commands.ReserveStockCommand;
+import io.eventuate.examples.tram.sagas.ordersandcustomers.products.api.replies.ProductNotFound;
+import io.eventuate.examples.tram.sagas.ordersandcustomers.products.api.replies.ProductStockLimitExceeded;
 import io.eventuate.tram.commands.consumer.CommandWithDestination;
 import io.eventuate.tram.sagas.orchestration.SagaDefinition;
 import io.eventuate.tram.sagas.simpledsl.SimpleSaga;
 
-import static io.eventuate.tram.commands.consumer.CommandWithDestinationBuilder.send;
-
 public class CreateOrderSaga implements SimpleSaga<CreateOrderSagaData> {
 
-  private OrderRepository orderRepository;
+	private OrderRepository orderRepository;
 
-  public CreateOrderSaga(OrderRepository orderRepository) {
-    this.orderRepository = orderRepository;
-  }
+	public CreateOrderSaga(OrderRepository orderRepository) {
+		this.orderRepository = orderRepository;
+	}
 
-  private SagaDefinition<CreateOrderSagaData> sagaDefinition =
-          step()
-            .invokeLocal(this::create)
-            .withCompensation(this::reject)
-          .step()
-            .invokeParticipant(this::reserveCredit)
-            .onReply(CustomerNotFound.class, this::handleCustomerNotFound)
-            .onReply(CustomerCreditLimitExceeded.class, this::handleCustomerCreditLimitExceeded)
-          .step()
-            .invokeLocal(this::approve)
-          .build();
+	private SagaDefinition<CreateOrderSagaData> sagaDefinition = 
+			step()
+				.invokeLocal(this::create).withCompensation(this::reject)
+			.step()
+				.invokeParticipant(this::reserveCredit)
+				.onReply(CustomerNotFound.class, this::handleCustomerNotFound)
+				.onReply(CustomerCreditLimitExceeded.class, this::handleCustomerCreditLimitExceeded)
+			.step()
+				.invokeParticipant(this::reserveStock)
+				.onReply(ProductNotFound.class, this::handleProductNotFound)
+				.onReply(ProductStockLimitExceeded.class, this::handleProductStockLimitExceeded)
+			.step()
+				.invokeLocal(this::approve).build();
 
-  private void handleCustomerNotFound(CreateOrderSagaData data, CustomerNotFound reply) {
-    data.setRejectionReason(RejectionReason.UNKNOWN_CUSTOMER);
-  }
+	private void handleCustomerNotFound(CreateOrderSagaData data, CustomerNotFound reply) {
+		data.setRejectionReason(RejectionReason.UNKNOWN_CUSTOMER);
+	}
 
-  private void handleCustomerCreditLimitExceeded(CreateOrderSagaData data, CustomerCreditLimitExceeded reply) {
-    data.setRejectionReason(RejectionReason.INSUFFICIENT_CREDIT);
-  }
+	private void handleCustomerCreditLimitExceeded(CreateOrderSagaData data, CustomerCreditLimitExceeded reply) {
+		data.setRejectionReason(RejectionReason.INSUFFICIENT_CREDIT);
+	}
 
+	private void handleProductNotFound(CreateOrderSagaData data, ProductNotFound reply) {
+		data.setRejectionReason(RejectionReason.UNKNOWN_PRODUCT);
+	}
 
-  @Override
-  public SagaDefinition<CreateOrderSagaData> getSagaDefinition() {
-    return this.sagaDefinition;
-  }
+	private void handleProductStockLimitExceeded(CreateOrderSagaData data, ProductStockLimitExceeded reply) {
+		data.setRejectionReason(RejectionReason.INSUFFICIENT_STOCK);
+	}
 
-  private void create(CreateOrderSagaData data) {
-    Order order = Order.createOrder(data.getOrderDetails());
-    orderRepository.save(order);
-    data.setOrderId(order.getId());
-  }
+	@Override
+	public SagaDefinition<CreateOrderSagaData> getSagaDefinition() {
+		return this.sagaDefinition;
+	}
 
-  private CommandWithDestination reserveCredit(CreateOrderSagaData data) {
-    long orderId = data.getOrderId();
-    Long customerId = data.getOrderDetails().getCustomerId();
-    Money orderTotal = data.getOrderDetails().getOrderTotal();
-    return send(new ReserveCreditCommand(customerId, orderId, orderTotal))
-            .to("customerService")
-            .build();
-  }
+	private void create(CreateOrderSagaData data) {
+		Order order = Order.createOrder(data.getOrderDetails());
+		orderRepository.save(order);
+		data.setOrderId(order.getId());
+	}
 
-  private void approve(CreateOrderSagaData data) {
-    orderRepository.findById(data.getOrderId()).get().approve();
-  }
+	private CommandWithDestination reserveCredit(CreateOrderSagaData data) {
+		long orderId = data.getOrderId();
+		Long customerId = data.getOrderDetails().getCustomerId();
+		Money orderTotal = data.getOrderDetails().getOrderTotal();
+		return send(new ReserveCreditCommand(customerId, orderId, orderTotal)).to("customerService").build();
+	}
 
-  public void reject(CreateOrderSagaData data) {
-    orderRepository.findById(data.getOrderId()).get().reject(data.getRejectionReason());
-  }
+	private CommandWithDestination reserveStock(CreateOrderSagaData data) {
+		long orderId = data.getOrderId();
+		Long productId = data.getOrderDetails().getProductId();
+		int orderUnits = data.getOrderDetails().getOrderUnits();
+		return send(new ReserveStockCommand(productId, orderId, orderUnits)).to("productService").build();
+	}
 
+	private void approve(CreateOrderSagaData data) {
+		orderRepository.findById(data.getOrderId()).get().approve();
+	}
 
+	public void reject(CreateOrderSagaData data) {
+		orderRepository.findById(data.getOrderId()).get().reject(data.getRejectionReason());
+	}
 
 }
